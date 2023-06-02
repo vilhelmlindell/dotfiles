6a15553970201fb73ae4da506f5dc525082c7648
Vilhelm Lindell <95926200+vilhelmlindell@users.noreply.github.com>
2023-06-02 15:03:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@ sudo pacman -S fish
 chsh -s $(which fish)
 curl -sS https://starship.rs/install.sh | sh
 
-yay -S npm
-yay -S rustup
-yay -S xclip
-yay -S spotify-launcher
-yay -S picom
-yay -S ripgrep
+pacman -S npm
+pacman -S rustup
+pacman -S xclip
+pacman -S spotify-launcher
+pacman -S picom
+pacman -S ripgrep
 rustup default stable
