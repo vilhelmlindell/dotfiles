e0bf8f7998e02af6c35f02d0b31363388098525f
Vilhelm <vilhelm.lindell123@gmail.com>
2023-09-22 23:16:43 +0200

    refactored

--- install.sh
+++ install.sh
@@ -1,11 +1,25 @@
-sudo pacman -S fish
-pacman -S which
+yay -S fish --noconfirm
+yay -S which --noconfirm
 chsh -s $(which fish)
 curl -sS https://starship.rs/install.sh | sh
 
-pacman -S npm
-pacman -S rustup
-pacman -S xclip
-pacman -S ripgrep
-pacman -S neovim
-rustup default stable
+yay -S picom --noconfirm
+
+yay -S neovim --noconfirm
+yay -S xclip --noconfirm
+yay -S ripgrep --noconfirm
+yay -S npm --noconfirm
+
+yay -S alacritty --noconfirm
+
+yay -S tmux --noconfirm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+yay -S rustup --noconfirm
+rustup default nightly
+
+ln -sf ~/dotfiles/fish ~/.config
+ln -sf ~/dotfiles/nvim ~/.config
+ln -sf ~/dotfiles/picom ~/.config
+ln -sf ~/dotfiles/tmux ~/.config
+ln -sf ~/dotfiles/alacritty ~/.config
