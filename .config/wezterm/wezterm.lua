b806fd99964964b938f39fb79fe051ba6f3ae588
Vilhelm <vilhelm.lindell123@gmail.com>
2024-08-15 21:25:21 +0200

    first commit

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("IosevkaTermNerdFontMono")
+config.font_size = 18.0
+config.color_scheme = "catppuccin-mocha"
+config.enable_tab_bar = false
+
+return config
