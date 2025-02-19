c40a49e19195c8dea711100ecbf9e6b7eb83c5e6
Vilhelm Lindell <vilhelm.lindell@gmail.com>
2025-02-19 15:01:34 +0100

    stuff

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'catppuccin)
+;; (setq doom-theme 'catppuccin)
+(load-theme 'gruber-darker-theme)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -77,11 +78,11 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(after! corfu
-  (corfu-popupinfo-mode 0)
-  ;; Optional customizations
-  (setq corfu-popupinfo-delay '(0 . 0)) ; Initial & subsequent delay
-  (setq corfu-popupinfo-hide nil))           ; Don't hide popup automatically
+;;(after! corfu
+;;  (corfu-popupinfo-mode 0)
+;;  ;; Optional customizations
+;;  (setq corfu-popupinfo-delay '(0 . 0)) ; Initial & subsequent delay
+;;  (setq corfu-popupinfo-hide nil))           ; Don't hide popup automatically
 
 (use-package! treesit-auto
   :custom
