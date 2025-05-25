3e041581f2e76c00f17a3940df4a8c8b6aef4949
Vilhelm Lindell <vilhelm.lindell123@gmail.com>
2025-05-25 17:34:08 +0200

    stuff

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require("wezterm")
 local config = {}
 local no_clown_fiesta, metadata = wezterm.color.load_base16_scheme("/home/vilhelm/dotfiles/.config/wezterm/no-clown-fiesta.yaml")
 
-config.font = wezterm.font("IosevkaTermNerdFont")
+config.font = wezterm.font {
+  family = 'JetBrains Mono',
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+}
 config.font_size = 28.0
 config.color_scheme = "(Gogh)"
 --config.color_scheme = "ayu"
@@ -11,5 +14,11 @@ config.color_scheme = "(Gogh)"
 config.colors = no_clown_fiesta 
 config.enable_tab_bar = false
 config.window_close_confirmation = 'NeverPrompt'
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 0,
+  bottom = 0,
+}
 
 return config
